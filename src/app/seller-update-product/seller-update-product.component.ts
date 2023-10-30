6f886ac588638ae1173edcd6ffe6a6bbc43688cc
shyamkryadav <shyamkryadv345@gmail.com>
2023-10-30 20:55:06 +0545

    createa update prouct page and creat url for update prodcut

---
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-update-product',
+  templateUrl: './seller-update-product.component.html',
+  styleUrls: ['./seller-update-product.component.css']
+})
+export class SellerUpdateProductComponent {
+
+
+  updateProducts(data:any){
+
+  }
+}
