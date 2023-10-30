4f063abaf1d2c8d1ae61ddb0fd9b93568b0fa59a
shyamkryadav <shyamkryadv345@gmail.com>
2023-10-30 22:49:18 +0545

    dynammic data shwo in carousel on page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  popularProducts:undefined| product[];
+  constructor(private product:ProductService){}
+
+  ngOnInit(): void {
+   this.product.popularProducts().subscribe((res)=>{
+    this.popularProducts=res
+   })
+  }
 }
