e2a7ef156f6de0050b7d8513c18b9dd72d487f42
shyamkryadav <shyamkryadv345@gmail.com>
2023-10-30 21:18:52 +0545

    prefilled the form data  in update form

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-update-product',
@@ -6,8 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-update-product.component.css']
 })
 export class SellerUpdateProductComponent {
+  productData:undefined | product;
 
+constructor (private route:ActivatedRoute , private product:ProductService){}
 
+ngOnInit(): void {
+  let ptoductId=this.route.snapshot.paramMap.get('id');
+  console.log(ptoductId)
+  ptoductId && this.product.getProduct(ptoductId).subscribe((res)=>{
+      this.productData=res
+  })
+}
   updateProducts(data:any){
 
   }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,21 +3,24 @@ import { Injectable } from '@angular/core';
 import { product } from '../data-type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   constructor(private http: HttpClient) {}
 
-  addProduct(data:product){
-   return this.http.post('http://localhost:3000/products',data);
+  addProduct(data: product) {
+    return this.http.post('http://localhost:3000/products', data);
   }
 
-  productList(){
+  productList() {
     return this.http.get<product[]>('http://localhost:3000/products');
   }
 
-  deleteProduct(id:number){
-  return this.http.delete(`http://localhost:3000/products/${id}`);
+  deleteProduct(id: number) {
+    return this.http.delete(`http://localhost:3000/products/${id}`);
+  }
+
+  getProduct(id: string) {
+    return this.http.get<product>(`http://localhost:3000/products/${id}`);
   }
 }
