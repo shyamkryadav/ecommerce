db10810320cdb3041c9a5c79e225c78a0421319a
shyamkryadav <shyamkryadv345@gmail.com>
2023-10-31 11:51:44 +0545

    make a user sign up page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { SellerAddProductComponent } from './seller-add-product/seller-add-produ
 import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
 import { SearchComponent } from './search/search.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
@@ -17,6 +18,7 @@ const routes: Routes = [
   {path:'seller-update-product/:id',component:SellerUpdateProductComponent,canActivate:[AuthGuard]},
   {path:'search/:query',component:SearchComponent},
   {path:'details/:productId',component:ProductDetailsComponent},
+  {path:'user-auth',component:UserAuthComponent},
 ];
 
 @NgModule({
---
+++ src/app/user-auth/user-auth.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SignUp } from '../data-type';
+
+@Component({
+  selector: 'app-user-auth',
+  templateUrl: './user-auth.component.html',
+  styleUrls: ['./user-auth.component.css']
+})
+export class UserAuthComponent {
+
+  singUp(data:SignUp){
+    
+  }
+}
